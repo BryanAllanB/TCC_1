15ad54fd3829f498aaff7df6b80b70743ce87014
BryanAllanB <bryanallanbraga@gamil.com>
2022-05-03 21:24:53 -0300

    Tcc

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+    //require "config.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TCC</title>
+    <base href="http://localhost/TCC/">
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/lightbox.min.css">
+</head>
+<body>
+    <header>
+        <a href="index.php" title="Home">
+            <img src="imagens/logo.png" alt="">
+        </a>
+    </header>
+    <footer>
+        <p>Desenvolvido por Bryan Allan</p>
+    </footer>
+</body>
+</html>
